24841abc5a7585ff75afaf30e5db047ef337afc8
Maksym <kotikmaxtop@gmail.com>
2026-02-05 11:05:39 +0000

    lesson4

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.main'
+    'apps.main',
+    'apps.accounts'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'config.middleware.AdminRedirectMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("apps.main.urls", namespace="main")),
+    path('accounts/', include('apps.accounts.urls', namespace='accounts'))
 ]
 
 if settings.DEBUG:
